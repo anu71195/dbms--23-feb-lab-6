3327ea46768269cc9d9c26f3db1b029c9ade855c
anu <anu71195@gmail.com>
2018-02-24 10:14:09 +0530

    cc and ett done

---
+++ 150101010.sql
@@ -0,0 +1,24 @@
+DROP DATABASE 150101010_23feb2018;
+CREATE DATABASE 150101010_23feb2018;
+USE 150101010_23feb2018;
+CREATE TABLE ett(
+	line_number int PRIMARY KEY AUTO_INCREMENT,
+	course_id varchar(6),
+	exam_date date,
+	start_time time,
+	end_time time,
+	UNIQUE(course_id,exam_date,start_time,end_time)
+);
+CREATE TABLE cc(
+	course_id varchar(6) PRIMARY KEY,
+	number_of_credits int
+);
+CREATE TABLE cwsl
+(
+	serial_number int AUTO_INCREMENT,
+	roll_number int,
+	name varchar(50),
+	email varchar(50),
+	course_id varchar(6),
+	PRIMARY KEY(serial_number)
+);
